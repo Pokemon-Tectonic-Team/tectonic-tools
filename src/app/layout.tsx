e3301e68153a7096e87db6002d7c74615da1f60f
AlphaKretin <deewhydeeecks@gmail.com>
2026-06-21 09:29:39 +0930

    Remove Vercel Analytics
    Irrelevant now that we self-host
    Fix #378

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-import { Analytics } from "@vercel/analytics/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -26,10 +25,7 @@ export default function RootLayout({
     return (
         <html lang="en">
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <body className={`${geistSans.variable} ${geistMono.variable}`}>
-                {children}
-                <Analytics />
-            </body>
+            <body className={`${geistSans.variable} ${geistMono.variable}`}>{children}</body>
         </html>
     );
 }
